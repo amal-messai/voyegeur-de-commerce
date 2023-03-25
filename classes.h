f9de5f29751e1e9f50f2d06802b0e6f98ae63491
amal-messai <128847914+amal-messai@users.noreply.github.com>
2023-03-25 04:29:30 +0100

    Update classes.h

--- classes.h
+++ classes.h
@@ -1,25 +1,25 @@
-#ifndef CLASSES_H_INCLUDED
-#define CLASSES_H_INCLUDED
+#ifndef CLASSE_H_INCLUDED
+#define CLASSE_H_INCLUDED
 
 #include <iostream>
 #include <string>
 
 using namespace std;
-
+const int taille = 42;
 class point
 {
     float x ;
     float y ;
     point(float a, float b): x(a), y(b) {};
 };
-class graphe ;
+class graphe
 {
 public :
     int dim;
     double** tab;
-    graphe( int n )
+    graphe( int n );
     ~graphe();
-    double val (int i , int j) {return tab[i][j]};
+    double val (int i , int j) {return tab[i][j];};
     void affiche();
 };
 
@@ -37,20 +37,26 @@ class individu
 public:
     int dim;
     double* indiv;
-    double* poids;
     individu(int n);
-    virtual individu (individu v)
-    ~individu();
-    virtual double adapt();
+    individu  (const individu& v);
+    individu();// default constructor
+    virtual~individu(){};
+    virtual double adapt()=0;
 
 };
 class chemin : public individu
 {
 public:
-    chemin(int n) : individu (n) {};
-    chemin (chemin v);
+    double* poids;
+    chemin(): individu(),poids(new double[taille]) {};
+    chemin(int n) : individu(n), poids(new double[n]) {} ;
+    chemin (const chemin& other) : individu(other), poids(new double[other.dim]) {
+        for (int i = 0; i < dim; i++) {
+            poids[i] = other.poids[i];
+        }
+    }
     double adapt ();
-
+    ~chemin();
 };
 class population
 {
@@ -58,10 +64,17 @@ public:
     int nbre;
     chemin* pop;
     population (int n );
-    chemin operator[] (int i ) {return chemin[i] };
+    population(const population& other);
+    ~population();
+    chemin& operator[] (int i ) {return pop[i];};
 
 };
 chemin mutation (chemin ch)  ;
-population* selection_prod (pipulation pop, string select);
+population* selection_prod (population pop, string select);
+// méthodes de séléction des reproducteurs
+chemin selec_roulette(population pop);
+chemin selec_rang(population pop);
+
+#endif CLASSE_H_INCLUDED
 
 
