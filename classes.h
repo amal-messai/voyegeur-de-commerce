983fc790d3f25e1cf074cd8c525cbf1d17c8c78f
amal-messai <128847914+amal-messai@users.noreply.github.com>
2023-03-26 05:38:51 +0200

    Update classes.h

--- classes.h
+++ classes.h
@@ -38,42 +38,41 @@ public:
     double* indiv;
     individu(int n);
     individu  (const individu& v);
-    individu()=default;// default constructor
+    individu()=default ;// default constructor
     virtual~individu(){};
-    virtual double adapt()=0;
+    virtual double adapt() const =0;
 
 };
 class chemin : public individu
 {
 public:
     double* poids;
-    chemin(): individu() {};
+    chemin(): individu(){};
     chemin(int n) : individu(n), poids(new double[n]) {} ;
     chemin (const chemin& other) : individu(other), poids(new double[other.dim]) {
         for (int i = 0; i < dim; i++) {
             poids[i] = other.poids[i];
         }
     }
-    double adapt ();
+    double adapt () const;
     ~chemin();
+    void affiche();
 };
 class population
 {
 public:
     int nbre;
-    vector<chemin> pop;
+    vector <chemin> pop;
     population (int n );
     population(const population& other);
     ~population();
-    chemin& operator[] (int i ) {return pop[i];};
+    chemin& operator[] (int i ) {return pop.at(i);};
 
 };
 chemin mutation (chemin ch)  ;
 population* selection_prod (population pop, string select);
 // méthodes de séléction des reproducteurs
 chemin selec_roulette(population pop);
-chemin selec_rang(population pop);
+chemin selec_rang(const population& pop);
 
 #endif CLASSE_H_INCLUDED
-
-
