4501a77c6c6c91601b05e33082dd5d3f18d4df0b
amal-messai <128847914+amal-messai@users.noreply.github.com>
2023-03-26 05:36:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
+#include "class.h"
 #include <iostream>
-
+#include <string>
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int k= (int)((float)rand() * (chemin.dim+1) / (RAND_MAX-1)) ;
-
-    cout << k<< endl;
+    chemin ch(5);
+    for (int i=0;i<ch.dim;i++)
+    {
+        ch.indiv[i]=i;
+    }
+    for (int i=0;i<ch.dim;i++)
+    {
+        ch.poids[i]=i*2;
+    }
+    cout<<ch.adapt()<<"\n";
+    chemin ch1(5);
+    for (int i=0;i<ch1.dim;i++)
+    {
+        ch1.indiv[i]=i*3;
+    }
+    for (int i=0;i<ch1.dim;i++)
+    {
+        ch1.poids[i]=i*5;
+    }
+    population popu(2);
+    population popu1(popu);
+    cout<<popu.nbre;
+    popu.pop[0]=ch;    popu.pop[1]=ch1;
+    chemin res=selec_roulette(popu); res.affiche();
     return 0;
 }
