46bf1f3853312193a29484f40d54afc0b5deafa3
amal-messai <128847914+amal-messai@users.noreply.github.com>
2023-04-10 17:11:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <cstdlib> // for rand() and srand()
 #include <ctime>   // for time()
 #include <bits/stdc++.h>
+#include <cstring>
+#include <vector>
 using namespace std;
-
 int main()
 {
     graphe G (4);
@@ -28,34 +29,32 @@ int main()
     G.tab[3][1]=25;
     G.tab[3][2]=30;
     G.tab[3][3]=-1;
-G.affiche();
-population pop_init=gen_init(G,5,0);
-pop_init.affiche();
-chemin ch= selec_roulette(pop_init,G);
-cout<<"\n"; cout<<"la selection par roulette donne "<<"\n";
-ch.affiche();
+population pop_init=gen_init(G,6,0);
 
-//population prod=selec_reproducteurs(pop_init,G,"selec_roulette");
-//population pop_next=selection_nextgen(prod,5,G);
-//pop_init=pop_next;
-////for (int i=0; i<10;i++)
-////{
-////    cout<<"ieration"<<i;
-////    delete &prod;
-////    delete &pop_next;
-////    prod=selec_reproducteurs(pop_init,G,"selec_roulette");
-////    pop_next=selection_nextgen(prod,5,G);
-////    delete &pop_init;
-////    population pop_init=pop_next;
-////
-////}
-//
-//
-//sort(pop_init.pop.begin(), pop_init.pop.end(), [&](const chemin& A, const chemin& B) {
-//        return compare_by_adapt_asc(A, B, G);
-//});
-//chemin chemin_meilleur(pop_init.pop[0]);
-//chemin_meilleur.affiche();
+population prod=selec_reproducteurs(pop_init,G,"selec_roulette");
+cout<<"ok2 "<<"\n";
+population pop_next=selection_nextgen(prod,pop_init,2,G);
+cout<<"ok3"<<"\n";
+pop_init=pop_next;
+cout<<"ok4"<<"\n ";
+
+
+for (int i=0; i<4;i++)
+{
+    cout<<"ieration"<<i;
+    prod=selec_reproducteurs(pop_init,G,"selec_roulette");
+    cout<<"reproducing  made";
+    pop_next=selection_nextgen(prod,pop_init,2,G);
+    cout<<"next gen "<<i<<"\n";
+    pop_init=pop_next;
+}
+pop_init.affiche();
+
+sort(pop_init.pop.begin(), pop_init.pop.end(), [&](const chemin& A, const chemin& B) {
+        return compare_by_adapt_asc(A, B, G);
+});
+chemin chemin_meilleur(pop_init.pop[0]);
+chemin_meilleur.affiche();
 
     return 0;
 }
